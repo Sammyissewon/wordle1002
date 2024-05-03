b3d45b3127219074f4c109f1a0e45d20cb09b202
Sammyissewon <119594882+Sammyissewon@users.noreply.github.com>
2024-05-03 18:25:07 +0700

    [wordle] JaveScript 로직 완성 ㅆㅂ
    간단한 워들게임을 '따라서' 만들었습니다...

---
+++ 29.index.js
@@ -0,0 +1,95 @@
+const 정답 = "TIGER";
+
+let attempts = 0;
+let index = 0;
+let timer;
+
+function appStart() {
+  const displayGameover = () => {
+    const div = document.createElement("div");
+    div.innerText = "게임이 종료되었습니다.";
+    div.style =
+      "display:flex; justify-content:center; align-items:center; position:absolute; top:40vh; left: 45vw; background-color:white; width:200px; height:100px;";
+    document.body.appendChild(div);
+  };
+
+  const gameover = () => {
+    window.removeEventListener("keydown", handleKeydown);
+    displayGameover();
+    clearInterval(timer);
+  };
+
+  const nextLine = () => {
+    if (attempts === 6) return gameover();
+    attempts += 1;
+    index = 0;
+  };
+
+  const handleEnterKey = () => {
+    let 맞은_갯수 = 0;
+    for (let i = 0; i < 5; i++) {
+      const block = document.querySelector(
+        `.board-block[data-index='${attempts}${i}']`
+      );
+      const 입력한_글자 = block.innerText;
+      const 정답_글자 = 정답[i];
+      if (입력한_글자 === 정답_글자) {
+        맞은_갯수 += 1;
+
+        block.style.background = "#6AAA64";
+      } else if (정답.includes(입력한_글자)) block.style.background = "#C9B458";
+      else block.style.background = "#787C7E";
+      block.style.color = "white";
+    }
+
+    if (맞은_갯수 === 5) gameover();
+    else nextLine();
+  };
+
+  const handleBackspace = () => {
+    if (index > 0) {
+      const preBlock = document.querySelector(
+        `.board-block[data-index='${attempts}${index - 1}']`
+      );
+      preBlock.innerText = "";
+    }
+    if (index !== 0) index -= 1;
+  };
+
+  const handleKeydown = (event) => {
+    const key = event.key.toUpperCase();
+    const keyCode = event.keyCode;
+    const thisBlock = document.querySelector(
+      `.board-block[data-index='${attempts}${index}']`
+    );
+
+    if (event.key === "Backspace") handleBackspace();
+    else if (index === 5) {
+      if (event.key === "Enter") handleEnterKey();
+      else return;
+    } else if (65 <= keyCode && keyCode <= 90) {
+      thisBlock.innerText = key;
+      index += 1;
+    }
+  };
+
+  const startTimer = () => {
+    const start = new Date();
+
+    function setTime() {
+      const current = new Date();
+      const passed = new Date(current - start);
+      const min = passed.getMinutes().toString().padStart(2, 0);
+      const sec = passed.getSeconds().toString().padStart(2, 0);
+      const timeDiv = document.querySelector("#timer");
+      timeDiv.innerText = `${min}:${sec}`;
+    }
+
+    timer = setInterval(setTime, 1000);
+  };
+
+  startTimer();
+  window.addEventListener("keydown", handleKeydown);
+}
+
+appStart();
